a74902d09c52fc603a400d82778bd128eea38dc5
EricLM.EVO <ericlm.evo@gmail.com>
2025-08-28 19:21:56 -0300

    Update BD.js
    UP

--- BD.js
+++ BD.js
@@ -1,6 +1,6 @@
 const users = [
-    { login: 'Portaria2025', password: 'Bloqueio@@1989', role: 'viewer' },
-    { login: 'Thallyson2025', password: 'Bloqueio@@1989', role: 'admin' },
-    { login: 'Daiana2025', password: 'Bloqueio@@1989', role: 'admin' },
+    { login: 'Portaria2025', password: 'Bloqueio@@19', role: 'viewer' },
+    { login: 'Adm2025', password: 'Bloqueio@@89', role: 'admin' },
+    { login: 'Thallyson2025', password: 'imile@@2023', role: 'admin' },
     { login: 'Ericlm.evo', password: 'Evo@@1313', role: 'admin' }
 ];
